f0fbb7f088bdef2b2256780e16b2cf25d07c6d28
Brazouskaya Darya <Darya.Brazouskaya@ensimag.imag.fr>
2015-06-24 09:47:21 +0200

    add: fake pour faire marcher les tests
    fonctions avec le corps vide pour pouvoir executer les tests

---
+++ kernel/fake.h
@@ -0,0 +1,13 @@
+#ifndef FAKE_H
+#define FAKE_H
+void clock_settings(unsigned long *quartz, unsigned long *ticks);
+unsigned long current_clock();
+void wait_clock(unsigned long clock);
+
+int cons_write(const char *str, long size);
+unsigned long cons_read(char *string, unsigned long length);
+void cons_echo(int on);
+void exit(int retval);
+int sys_info();
+
+#endif
